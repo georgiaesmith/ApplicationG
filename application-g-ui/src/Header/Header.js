a67226356be7f73fc76847b10c7eba995b07c43d
georgiasmith <georgiaesmith@live.co.uk>
2022-01-09 20:20:00 +0000

    Recreating Header .js and .css files in the correct folder structure

---
+++ application-g-ui/src/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import Logo from './Logo/Logo';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Fitness Tracker</h1>
+      <Logo></Logo>
+    </div>
+  );
+}
+
+export default Header;
