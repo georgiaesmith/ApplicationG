221e4c78a4698c24b4fb9acfbe301104276c9107
georgiasmith <georgiaesmith@live.co.uk>
2022-01-09 20:58:05 +0000

    creating a content component

---
+++ application-g-ui/src/Content/Content.js
@@ -0,0 +1,11 @@
+import './Content.css';
+
+function Content() {
+    return (
+      <div className="Content">
+          <p>I am the content</p>
+      </div>
+    );
+  }
+  
+  export default Content;
