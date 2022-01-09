46341272711fef35921fe27de8a3f908668e6c93
georgiasmith <georgiaesmith@live.co.uk>
2022-01-09 20:31:35 +0000

    add an ID to the logo and style it accordingly

--- application-g-ui/src/Header/Logo/Logo.js
+++ application-g-ui/src/Header/Logo/Logo.js
@@ -3,7 +3,7 @@ import './Logo.css';
 function Logo() {
   return (
     <div className="Logo">
-      <img src={require('./FitnessLogo.png')} />
+      <img id="fitness-logo" src={require('./FitnessLogo.png')} />
     </div>
   );
 }
