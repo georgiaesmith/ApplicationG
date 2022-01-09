d2bc766dda8b108d3648a2198b8ea2d1ea529652
georgiasmith <georgiaesmith@live.co.uk>
2022-01-09 20:58:23 +0000

    implementing content component tags in to the app.js file

--- application-g-ui/src/App.js
+++ application-g-ui/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header/Header';
+import Content from './Content/Content';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Content></Content>
     </div>
   );
 }
